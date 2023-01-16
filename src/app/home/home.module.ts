521db0287d711df37c41d5d62fc2404b6faa9156
Job Karani <jobkarani084j@gmail.com>
2023-01-16 15:02:52 +0300

    creates home module category component

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,13 +4,15 @@ import { IndexComponent } from './index/index.component';
 import { HomeComponent } from './home.component';
 import { SharedModule } from '../shared/shared.module';
 import { HomeRoutingModule } from './home-routing.module';
+import { CategoriesComponent } from './categories/categories.component';
 
 
 
 @NgModule({
   declarations: [
     HomeComponent,
-    IndexComponent
+    IndexComponent,
+    CategoriesComponent
   ],
   imports: [
     CommonModule,
