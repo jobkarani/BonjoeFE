a33e0075fcf4caffad8dde86b4355eb19d41a417
Job Karani <jobkarani084j@gmail.com>
2023-01-16 13:29:58 +0300

    creates homecategories service

--- src/app/services/homecategories.service.ts
+++ src/app/services/homecategories.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { Category } from '../interfaces/category';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HomecategoriesService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getCategories() {
+    return this.http.get<Category[]>('https://bonjoebe-api-production.up.railway.app/api_categories/').pipe(
+      map(category => category)
+    );
+  }
 }
