32de2e57bf24f97894ee219fc53a09b0cce9d2c7
Job Karani <jobkarani084j@gmail.com>
2023-01-16 13:09:34 +0300

    creates interface category

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,14 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit{
 
-  navBarFunction() {
-    document.getElementsByClassName("navbar")[0].classList.toggle("responsive");
+  constructor() { }
+
+  ngOnInit(): void {
+    $(document).ready(function () {
+      $(window).scroll(function () {
+                var height = $(".navigation").height();
+                var scrollTop = $(window).scrollTop();
+                console.log("Scroll");
+      
+                if (scrollTop! >= height! - 70) {
+                  $(".navigation").css("background-color", "#251104");
+                } else {
+                  $(".navigation").css("background-color", "transparent");
+                }
+              });
+      });
   }
 
 }
