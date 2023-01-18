0246e052fba68be9ec15f80799b7f761da33d024
Job Karani <jobkarani084j@gmail.com>
2023-01-18 15:52:31 +0300

    creates productdetails service

--- src/app/shop/product-details/product-details.component.ts
+++ src/app/shop/product-details/product-details.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from 'src/app/interfaces/product';
+import { ProductdetailsService } from 'src/app/services/productdetails.service';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.css']
 })
-export class ProductDetailsComponent {
+export class ProductDetailsComponent implements OnInit{
+
+  singleProduct:Product[] = [];
+  id:number = 0;
+
+  constructor(private productdetailsService:ProductdetailsService, private route:ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe(
+      data =>{
+        this.id = data['id'];
+        console.log(this.id);
+        this.productdetailsService.getProductDetails(this.id).subscribe(
+          datax =>{
+            this.singleProduct = datax;
+            console.log(datax)
+          }
+        )
+      }
+    );
+  }
 
 }
