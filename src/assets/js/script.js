d06b21b6ed139b55eb95c53dfa3899fc9dc7c73a
Job Karani <jobkarani084j@gmail.com>
2023-03-14 00:05:49 +0300

    feat: contact component done

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,3 +1,18 @@
-// $(window).on('scroll', function() {
-//     $('nav').toggleClass('scrolled', $(this).scrollTop() > 50);
-//   });
+// form data to whatsapp
+function whatsapp(){
+    var firstname = document.getElementById("first-name").value;
+    var lastname = document.getElementById("last-name").value;
+    var emailinput = document.getElementById("email-input").value;
+    var Phone = document.getElementById("phone-input").value;
+    var Message = document.getElementById("message-textarea").value;
+  
+    var url = "https://wa.me/254724087213?text="
+    +"* First Name : * " + firstname + "%0a"
+    +"* Last Name : * " + lastname + "%0a"
+    +"* Email : * " + emailinput + "%0a"
+    +"* Phone : * " + Phone + "%0a"
+    +"* Message : * " + Message + "%0a"
+  
+    window.open(url, '_blank').focus;
+  
+  }
