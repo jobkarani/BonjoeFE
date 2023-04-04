c9606843e17a5d76e0808ed19a2b37fa4abb3875
Job Karani <jobkarani084j@gmail.com>
2023-04-04 21:59:04 +0300

    feat: deployment

--- src/app/home/top-deals/top-deals.component.ts
+++ src/app/home/top-deals/top-deals.component.ts
@@ -23,6 +23,6 @@ export class TopDealsComponent implements OnInit{
 
   // whatsapp 
   openWhatsApp( index:number) {
-    window.open(`https://wa.me/${this.phonenumber}?text=Hello%2C%20I%20want%20to%20purchase%3A%0D%0A%0D%0A%20%20%20%20%20%20%20%20%20%C2%A0*Buy:*%20${this.topDeals[index].name}%0A%20%20%20%20%20%20%20%20*Price:*%20KSh${this.topDeals[index].new_price}%0A%20%20%20%20%20%20%20%20*URL:*%20https://40163de3.ahericareng.pages.dev/product-details/${this.topDeals[index].id}%2F%0D%0A%0D%0AThank%20you%21`, "_blank");
+    window.open(`https://wa.me/${this.phonenumber}?text=Hello%2C%20I%20want%20to%20purchase%3A%0D%0A%0D%0A%20%20%20%20%20%20%20%20%20%C2%A0*Buy:*%20${this.topDeals[index].name}%0A%20%20%20%20%20%20%20%20*Price:*%20KSh${this.topDeals[index].new_price}%0A%20%20%20%20%20%20%20%20*URL:*%20https://bonjoe-fe.vercel.app/shop/product-details/${this.topDeals[index].id}%2F%0D%0A%0D%0AThank%20you%21`, "_blank");
   }
 }
--- src/app/home/top-selling/top-selling.component.ts
+++ src/app/home/top-selling/top-selling.component.ts
@@ -23,7 +23,7 @@ export class TopSellingComponent implements OnInit{
 
   // whatsapp 
   openWhatsApp( index:number) {
-    window.open(`https://wa.me/${this.phonenumber}?text=Hello%2C%20I%20want%20to%20purchase%3A%0D%0A%0D%0A%20%20%20%20%20%20%20%20%20%C2%A0*Buy:*%20${this.topSales[index].name}%0A%20%20%20%20%20%20%20%20*Price:*%20KSh${this.topSales[index].new_price}%0A%20%20%20%20%20%20%20%20*URL:*%20https://40163de3.ahericareng.pages.dev/product-details/${this.topSales[index].id}%2F%0D%0A%0D%0AThank%20you%21`, "_blank");
+    window.open(`https://wa.me/${this.phonenumber}?text=Hello%2C%20I%20want%20to%20purchase%3A%0D%0A%0D%0A%20%20%20%20%20%20%20%20%20%C2%A0*Buy:*%20${this.topSales[index].name}%0A%20%20%20%20%20%20%20%20*Price:*%20KSh${this.topSales[index].new_price}%0A%20%20%20%20%20%20%20%20*URL:*%20https://bonjoe-fe.vercel.app/shop/product-details/${this.topSales[index].id}%2F%0D%0A%0D%0AThank%20you%21`, "_blank");
   }
 
 }
